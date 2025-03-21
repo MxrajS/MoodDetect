9702fd01c19f970d54962112668269b6211805b8
AlexDogariu1 <dogariu.a22@htlwienwest.at>
2025-03-21 09:22:35 +0100

    Zwischenstand (Kamera richtig stellen)

--- models_script.js
+++ models_script.js
@@ -13,17 +13,31 @@ async function start() {
     video.addEventListener('play', async () => {
         const canvas = faceapi.createCanvasFromMedia(video);
         document.body.append(canvas);
+        const ctx = canvas.getContext('2d');
         const displaySize = { width: video.width, height: video.height };
         faceapi.matchDimensions(canvas, displaySize);
 
         setInterval(async () => {
             const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
                 .withFaceExpressions();
-
+        
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            
+            ctx.save();
+            ctx.translate(canvas.width, 0); 
+            ctx.scale(-1, 1); // Spiegelt das Bild
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+            ctx.restore(); // Kontext zurücksetzen
+        
+            faceapi.draw.drawDetections(canvas, detections);
+            faceapi.draw.drawFaceExpressions(canvas, detections);
+        
             if (detections.length > 0) {
                 console.log("Erkannte Emotionen:", detections[0].expressions);
             }
         }, 500);
+        
     });
 }
+
 start();
